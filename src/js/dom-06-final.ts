49c8278902260e131a5173eeab7bd29245dac09d
artaff <art911191@gmail.com>
2023-07-10 17:16:15 +0700

    origin

---
+++ src/js/dom-06-final.ts
@@ -0,0 +1,5 @@
+import { createComponent } from "./input-component.js";
+
+createComponent(document.body);
+
+  // npx http-server -c0 ()
---
+++ src/js/input-component.ts
@@ -0,0 +1,86 @@
+export function createComponent(componentElement: HTMLElement) {
+    const tempInput = componentElement.querySelector<HTMLTemplateElement>("template.app-temp-input");
+  
+    if(tempInput === null) {
+      throw new Error(`Cannot find input template`);
+    }
+
+    const inputList = tempInput.parentElement;
+
+    if(inputList === null) { 
+      throw new Error(`Cannot find list container`);
+    }
+
+    const updateResult = () => {
+      const children = [...inputList.children].filter(
+        (elem) => elem !== tempInput
+      );
+  
+      const result = children.reduce(
+        (carry, element) =>
+          carry +
+          (element.querySelector<HTMLInputElement>('input[type="number"].app-cmp-input')?.valueAsNumber ?? 0),
+        0,
+      );
+  
+      [...componentElement.querySelectorAll<HTMLOutputElement>('output.app-cmp-result')].forEach(
+        (elem) => (elem.value = `${result.toLocaleString()}`),
+      );
+    };
+  
+    const updateList = () => {
+      updateResult();
+  
+      const children = [...inputList.children].filter(
+        (elem) => elem !== tempInput
+      );
+  
+      children.forEach((element, i) => {
+        [...element.querySelectorAll(".app-cmp-input-no")].forEach((elem) => {
+          elem.textContent = `${i + 1}`;
+        });
+      });
+  
+      [...inputList.querySelectorAll<HTMLElement & { disabled: boolean}>(".app-cmd-remove-input")].forEach(
+        (elem) => (elem.disabled = children.length === 1)
+      );
+    };
+  
+    const createElement = () => {
+      const container = (tempInput.content.cloneNode(true) as DocumentFragment).firstElementChild;
+      
+      if(container ==null) {
+        throw new Error(`Cannot find template container`);
+      }
+
+      container.addEventListener("click", (ev) => {
+        if ((ev.target as Element | null)?.matches(".app-cmd-remove-input")) {
+          container.remove();
+          updateList();
+        }
+      });
+  
+      container
+        .querySelector('input[type="number"].app-cmp-input')
+        ?.addEventListener("change", updateResult);
+  
+      return container;
+    };
+  
+    componentElement.addEventListener("click", (ev) => {
+      if ((ev.target as Element | null)?.matches(".app-cmp-add-input")) {
+        inputList.append(createElement());
+        updateList();
+      }
+    });
+  
+    inputList.addEventListener("change", (ev) => {
+      if ((ev.target as Element | null)?.matches('input[type="number"].app-cmp-input')) {
+        updateResult();
+      }
+    });
+  
+    inputList.append(createElement());
+    updateList();
+  }
+  
