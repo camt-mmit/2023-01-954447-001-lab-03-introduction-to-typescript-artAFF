e94430cbd90dad977409901d26fd5cfcd3801ac9
artaff <art911191@gmail.com>
2023-07-17 19:11:16 +0700

    origin

---
+++ src/js/dom-lab.ts
@@ -0,0 +1,6 @@
+import { createSectionComponent } from "./section-component.js";
+
+
+createSectionComponent(document.body);
+
+// npx gh-pages -d src
---
+++ src/js/section-component.ts
@@ -0,0 +1,61 @@
+
+
+export function createSectionComponent(componentElement: HTMLElement) {
+  const tempInput = componentElement.querySelector<HTMLTemplateElement>("template.app-temp-section");
+
+  if(tempInput === null) {
+    throw new Error(`Cannot find input template`);
+  }
+
+  const inputList = tempInput.parentElement;
+
+  if(inputList === null) { 
+    throw new Error(`Cannot find list container`);
+  }
+
+  const updateList = () => {
+
+    const children = [...inputList.children].filter(
+      (elem) => elem !== tempInput
+    );
+
+    children.forEach((element, i) => {
+      [...element.querySelectorAll(".app-cmp-add-section-no")].forEach((elem) => {
+        elem.textContent = `${i + 1}`;
+      });
+    });
+
+    [...inputList.querySelectorAll(".app-cmd-remove-section")].forEach(
+      (elem) => (elem.disabled = children.length === 1)
+    );
+  };
+
+  const createElement = () => {
+    const container = (tempInput.content.cloneNode(true) as DocumentFragment).firstElementChild;
+
+    if(container ==null) {
+      throw new Error(`Cannot find template container`);
+    }
+
+    container.addEventListener("click", (ev) => {
+      if ((ev.target as Element | null)?.matches(".app-cmd-remove-section")) {
+        (ev.currentTarget as Element | null)?.remove();
+        updateList();
+      }
+    });
+
+    return container;
+  };
+
+  componentElement.addEventListener("click", (ev) => {
+    if ((ev.target as Element | null)?.matches(".app-cmp-add-section")) {
+      inputList.append(createElement());
+      updateList();
+    }
+  });
+
+  inputList.append(createElement());
+  updateList();
+}
+
+// npx http-server -c0 ()
